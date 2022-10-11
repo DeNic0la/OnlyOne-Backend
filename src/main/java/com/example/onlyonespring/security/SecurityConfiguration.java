8481ac9b4a9d38703856e06b45d5e59ee8485ac5
DeNicola <nicola@fioretti.info>
2022-10-12 00:43:53 +0200

    add sekiiiiuriiiiitiiii

---
+++ src/main/java/com/example/onlyonespring/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.onlyonespring.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration { //TODO: ALEXXXXXXXXX YOU DO THAT SHIT I HAVENT DONE THAT IN SPRING LIKE EVER
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2**", "/ignore2");
+    }
+
+}
