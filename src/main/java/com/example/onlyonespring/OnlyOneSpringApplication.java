c83c2b9c2fb169afa69d5a730d943b8688d3de6a
DeNicola <nicola@fioretti.info>
2022-10-12 00:27:58 +0200

    init

---
+++ src/main/java/com/example/onlyonespring/OnlyOneSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlyonespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyOneSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyOneSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlyonespring/OnlyOneSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlyonespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyOneSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
