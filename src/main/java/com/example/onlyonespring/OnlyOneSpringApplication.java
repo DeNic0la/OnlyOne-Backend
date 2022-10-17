eee26357abf21f3df660715f36a070c1c48de44c
DeNicola <nicola@fioretti.info>
2022-10-17 22:13:16 +0200

    fix cors stuff and add todo for someone else to fix it

--- src/main/java/com/example/onlyonespring/OnlyOneSpringApplication.java
+++ src/main/java/com/example/onlyonespring/OnlyOneSpringApplication.java
@@ -2,6 +2,9 @@ package com.example.onlyonespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OnlyOneSpringApplication {
@@ -10,4 +13,13 @@ public class OnlyOneSpringApplication {
         SpringApplication.run(OnlyOneSpringApplication.class, args);
     }
 
+    @Bean // TODO: ALEX or someone else, idc: clean this up, this probably should not be here but it works so idc.
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
