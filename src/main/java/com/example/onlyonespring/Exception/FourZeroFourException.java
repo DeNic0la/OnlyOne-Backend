a89bacf65ceea2a2c15bacc821f1137f4a6cddef
DeNicola <nicola@fioretti.info>
2022-10-17 20:27:50 +0200

    add exception

---
+++ src/main/java/com/example/onlyonespring/Exception/FourZeroFourException.java
@@ -0,0 +1,8 @@
+package com.example.onlyonespring.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason="Something was not found, good luck figuring out what lmao")
+public class FourZeroFourException extends RuntimeException{
+}
