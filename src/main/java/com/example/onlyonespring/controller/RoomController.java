9a89d918393e2ec44896052f076b5a6a949736f6
DeNicola <nicola@fioretti.info>
2022-10-17 19:55:23 +0200

    add Post method

--- src/main/java/com/example/onlyonespring/controller/RoomController.java
+++ src/main/java/com/example/onlyonespring/controller/RoomController.java
@@ -1,10 +1,10 @@
 package com.example.onlyonespring.controller;
 
 import com.example.onlyonespring.entity.Room;
+import com.example.onlyonespring.repository.RoomRepository;
 import lombok.val;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,29 +12,23 @@ import java.util.List;
 @RestController
 public class RoomController {
 
+    @Autowired
+    private RoomRepository roomRepository;
 
     @GetMapping("/room")
     public List<Room> getAllRooms(){
-        ArrayList<Room> rooms = new ArrayList<>();
-        Room room1 = this.getExampleRoom(1);
-        Room room2 = this.getExampleRoom(2);
-        Room room3 = this.getExampleRoom(3);
-
-        room1.setStatus("lobby");
-        room2.setStatus("run");
-        room3.setStatus("finished");
-        rooms.add(room1);
-        rooms.add(room2);
-        rooms.add(room3);
-        return rooms;
+        return roomRepository.findAll();
     }
 
-    private Room getExampleRoom(int random){
+    @PostMapping("/room")
+    public Room createRoom(@RequestBody String roomName){
         val room = new Room();
-        room.setName("Toms cool people club "+ random);
-        room.setId(random);
-        room.setPlayer_count(random);
+        room.setName(roomName);
+        room.setPlayer_count(0);
         room.setMax_player_count(10);
-        return room;
+        room.setStatus("lobby");
+        Room savedRoom = roomRepository.saveAndFlush(room);
+        return savedRoom;
     }
+
 }
