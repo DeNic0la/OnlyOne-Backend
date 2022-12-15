5ef3791a3b70d7ee2d50108724622274a2c2442e
alex-skxy <70218821+alex-skxy@users.noreply.github.com>
2022-12-15 14:22:46 +0000

    do not use slang in code

--- backend/src/main/java/com/example/onlyonespring/Exception/FourZeroFourException.java
+++ backend/src/main/java/com/example/onlyonespring/Exception/FourZeroFourException.java
@@ -3,6 +3,6 @@ package com.example.onlyonespring.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason="Something was not found, good luck figuring out what lmao")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason="Something was not found, good luck figuring out what")
 public class FourZeroFourException extends RuntimeException{
 }
